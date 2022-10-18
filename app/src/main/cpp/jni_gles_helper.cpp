1f1f539df6921aafe9413430a6decd898aaa9cc8
lpf <569307092@qq.com>
2022-10-19 00:38:42 +0800

    增加绘制点和线，启用平滑功能

--- app/src/main/cpp/jni_gles_helper.cpp
+++ app/src/main/cpp/jni_gles_helper.cpp
@@ -31,6 +31,8 @@ void DrawTextureMap();
 GLuint createOpenGLTexture(MSImage* pImg);
 GLuint createOpenGLTexture2(NativeImage* pImg);
 void createTextureIDs();
+void drawCubeTextureMap();
+void drawLineAndPoint();
 
 float mAngle =0.0f;
 GLuint m_texID; //纹理id
@@ -61,7 +63,7 @@ Java_com_meishe_msopengles2_MSGLRender_jniGLResize(JNIEnv *env, jobject thiz, ji
     glLoadIdentity();
 
     glOrthof(-1,1,-1,1,0.1,1000.0);  //正交矩阵
-    //glFrustumf(-1,1,-1,1,0.1,1000.0);  //透视矩阵
+//    glFrustumf(-1,1,-1,1,0.1,1000.0);  //透视矩阵
 //    glMatrixMode(GL_MODELVIEW); //GL_MODELVIEW是对模型视景矩阵操作
 //    glLoadIdentity();
 
@@ -76,101 +78,14 @@ Java_com_meishe_msopengles2_MSGLRender_jniGLDraw(JNIEnv *env, jobject thiz) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //清理颜色和深度缓冲buffer
     glLoadIdentity();
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 
-    glCullFace(GL_BACK);
-    /*函数原型
-void glFrontFace(GLenum mode);
-作用是控制多边形的正面是如何决定的。在默认情况下，mode是GL_CCW。
-mode的值为：
-GL_CCW 表示窗口坐标上投影多边形的顶点顺序为逆时针方向的表面为正面。
-GL_CW 表示顶点顺序为顺时针方向的表面为正面。
-顶点的方向又称为环绕。*/
-    glFrontFace(GL_CW);
-
-    MSFloat5 planVertices[] =
-            {
-                    { -1.0f, -1.0f, 1.0f,    0,  0 },
-                    {  -1.0f,1.0f, 1.0f,   0,  1 },
-                    {  1.0f,-1.0f, 1.0f,    1,  0 },
-                    {  1.0f, 1.0f, 1.0f,    1,  1 },
-            };
-
-    MSFloat5  cubeVertexs[] = {
-            { -0.5f, -0.5f, -0.5f,  0.0f, 0.0f},
-            { 0.5f, -0.5f, -0.5f,  1.0f, 0.0f},
-            { 0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
-            { 0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
-            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f},
-            {-0.5f, -0.5f, -0.5f,  0.0f, 0.0f},
-
-            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
-            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f, 1.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f, 1.0f},
-            {-0.5f,  0.5f,  0.5f,  0.0f, 1.0f},
-            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
-
-            {-0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-            {-0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
-            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
-            {-0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-
-            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-            {0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
-            {0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-            {0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-            {0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-
-            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-            {0.5f, -0.5f, -0.5f,  1.0f, 1.0f},
-            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
-            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
-            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
-            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
-
-            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f},
-            {0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
-            {-0.5f,  0.5f,  0.5f,  0.0f, 0.0f},
-            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f}
-    };
-
-
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-    glVertexPointer(3,GL_FLOAT,sizeof (MSFloat5),cubeVertexs);
-    glTexCoordPointer(2,GL_FLOAT,sizeof (MSFloat5),&cubeVertexs[0].u);
-
-    mAngle+=0.02;
-
-    glm::mat4x4 cubeMat;
-    glm::mat4x4 cubeTransMat=glm::translate(glm::mat4(1.0f),glm::vec3(0.0f, 0.0f, -0.5));
-    glm::mat4x4 cubeRotMat=glm::rotate(glm::mat4(1.0f),mAngle,glm::vec3(1.0f, 1.0f, 1.0));
-    glm::mat4x4 cubeScaleMat=glm::scale(glm::mat4(1.0f),glm::vec3(0.6f, 0.6f, 0.5));
-
-    cubeMat=cubeTransMat*cubeRotMat*cubeScaleMat;
-
-    glLoadMatrixf(glm::value_ptr(cubeMat));
-
-    //因为立方体有6个面，所以需要循环6次，每次绘制一个不同的面
-    for (int i = 0; i < 6; ++i) {
-        glBindTexture(GL_TEXTURE_2D, m_texIDs[i]); //生成不同的纹理id就可以给每个面添加不同的纹理贴图了
-        glDrawArrays(GL_TRIANGLES,i*6,6);
-    }
-
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
 //    drawTriangleStrip();
 //    drawCube();
 //    DrawTextureMap();
+//    drawCubeTextureMap();
+//     drawTextureMapCombine();
+//     drawLineAndPoint();
 
 }
 
@@ -301,6 +216,41 @@ GLuint createOpenGLTexture2(NativeImage* pImg){
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, pImg->width, pImg->height,
                  0, GL_RGBA, GL_UNSIGNED_BYTE, pImg->ppPlane[0]);
     return textureID;
+}
+
+/**
+ * 绘制线和点  包含平滑的使用 以及平滑采样等
+ */
+void drawLineAndPoint(){
+    MSFloat7 vertexTriangle[]={
+            {-0.5f,-0.5f,-1,1.0f,0.0f,0.0f,1.0f},
+            {-0.5f,0.5f,-1,0.0f,1.0f,0.0f,1.0f},
+            {0.5f,0.5f,-1,1.0f,0.0f,0.0f,1.0f},
+            {0.5f,-0.5f,-1,0.0f,0.0f,1.0f,1.0f},
+    };
+
+//    glColor4f(0.0f,0.0f,1.0f,1.0f);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3,GL_FLOAT,sizeof (MSFloat7),vertexTriangle);
+
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer(4,GL_FLOAT,sizeof (MSFloat7),&vertexTriangle[0].r);
+
+    glLineWidth(12);
+    glEnable(GL_LINE_SMOOTH);
+    glHint(GL_LINE_SMOOTH_HINT,GL_NICEST);  //最好的的平滑方式
+
+    glDrawArrays(GL_LINE_LOOP,0,4); //绘制线
+
+    //开启点抗锯齿
+    glPointSize(40);
+    glEnable(GL_POINT_SMOOTH);
+    glHint(GL_POINT_SMOOTH_HINT,GL_NICEST); //点如果不开启平滑就变成矩形了，  开启平滑是原点
+
+    glDrawArrays(GL_POINTS,0,4); //绘制点
+
+    glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 /**
@@ -469,6 +419,163 @@ void DrawTextureMap(){
     glLoadMatrixf(glm::value_ptr(cubeMat));
 
     glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+
+/**
+ * 绘制立方体，每个面绘制不同的图片
+ */
+void drawCubeTextureMap(){
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glCullFace(GL_BACK);
+    /*函数原型
+    void glFrontFace(GLenum mode);
+    作用是控制多边形的正面是如何决定的。在默认情况下，mode是GL_CCW。
+    mode的值为：
+    GL_CCW 表示窗口坐标上投影多边形的顶点顺序为逆时针方向的表面为正面。
+    GL_CW 表示顶点顺序为顺时针方向的表面为正面。
+    顶点的方向又称为环绕。*/
+    glFrontFace(GL_CW);
+
+    MSFloat5 planVertices[] =
+            {
+                    { -1.0f, -1.0f, 1.0f,    0,  0 },
+                    {  -1.0f,1.0f, 1.0f,   0,  1 },
+                    {  1.0f,-1.0f, 1.0f,    1,  0 },
+                    {  1.0f, 1.0f, 1.0f,    1,  1 },
+            };
+
+    MSFloat5  cubeVertexs[] = {
+            { -0.5f, -0.5f, -0.5f,  0.0f, 0.0f},
+            { 0.5f, -0.5f, -0.5f,  1.0f, 0.0f},
+            { 0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
+            { 0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
+            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f,  0.0f, 0.0f},
+
+            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
+            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f, 1.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f, 1.0f},
+            {-0.5f,  0.5f,  0.5f,  0.0f, 1.0f},
+            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
+
+            {-0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+            {-0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
+            {-0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+
+            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+            {0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
+            {0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+            {0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+            {0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+
+            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+            {0.5f, -0.5f, -0.5f,  1.0f, 1.0f},
+            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
+            {0.5f, -0.5f,  0.5f,  1.0f, 0.0f},
+            {-0.5f, -0.5f,  0.5f,  0.0f, 0.0f},
+            {-0.5f, -0.5f, -0.5f,  0.0f, 1.0f},
+
+            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f},
+            {0.5f,  0.5f, -0.5f,  1.0f, 1.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f, 0.0f},
+            {-0.5f,  0.5f,  0.5f,  0.0f, 0.0f},
+            {-0.5f,  0.5f, -0.5f,  0.0f, 1.0f}
+    };
+
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    glVertexPointer(3,GL_FLOAT,sizeof (MSFloat5),cubeVertexs);
+    glTexCoordPointer(2,GL_FLOAT,sizeof (MSFloat5),&cubeVertexs[0].u);
+
+    mAngle+=0.02;
+
+    glm::mat4x4 cubeMat;
+    glm::mat4x4 cubeTransMat=glm::translate(glm::mat4(1.0f),glm::vec3(0.0f, 0.0f, -0.5));
+    glm::mat4x4 cubeRotMat=glm::rotate(glm::mat4(1.0f),mAngle,glm::vec3(1.0f, 1.0f, 1.0));
+    glm::mat4x4 cubeScaleMat=glm::scale(glm::mat4(1.0f),glm::vec3(0.6f, 0.6f, 0.5));
+
+    cubeMat=cubeTransMat*cubeRotMat*cubeScaleMat;
+
+    glLoadMatrixf(glm::value_ptr(cubeMat));
+
+    //因为立方体有6个面，所以需要循环6次，每次绘制一个不同的面
+    for (int i = 0; i < 6; ++i) {
+        glBindTexture(GL_TEXTURE_2D, m_texIDs[i]); //生成不同的纹理id就可以给每个面添加不同的纹理贴图了
+        glDrawArrays(GL_TRIANGLES,i*6,6);
+    }
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+
+/**
+ * 绘制两个纹理进行合并
+ */
+void drawTextureMapCombine(){
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CW);
+
+    MSFloat5 planVertices[]={
+            {  1.0f, 1.0f, 1.0f,    1,  1 },
+            {  1.0f,-1.0f, 1.0f,    1,  0 },
+            {  -1.0f,1.0f, 1.0f,   0,  1 },
+            { -1.0f, -1.0f, 1.0f,    0,  0 }
+    };
+
+    glActiveTexture(GL_TEXTURE0);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D,m_texIDs[3]);
+
+
+    glActiveTexture(GL_TEXTURE1);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D,m_texIDs[5]);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3,GL_FLOAT,sizeof (MSFloat5),
+                    planVertices); //绑定顶点数组数据
+
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glClientActiveTexture(GL_TEXTURE0);
+    glTexCoordPointer(2,GL_FLOAT,sizeof (MSFloat5),&planVertices[0].u);
+
+
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glClientActiveTexture(GL_TEXTURE1);
+    glTexCoordPointer(2,GL_FLOAT,sizeof (MSFloat5),&planVertices[0].u);
+
+
+    glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_COMBINE);
+//    glTexEnvi(GL_TEXTURE_ENV,GL_COMBINE_RGB,GL_MODULATE);  //表示两个纹理相乘
+    glTexEnvi(GL_TEXTURE_ENV,GL_COMBINE_RGB,GL_ADD);  //表示两个纹理做加法
+
+//    mAngle += 0.01f;
+
+    glm::mat4x4 cubeMat;
+    glm::mat4x4 cubeTransMat=glm::translate(glm::mat4(1.0f),glm::vec3(0.0f, 0.0f, -0.5));
+    glm::mat4x4 cubeRotMat=glm::rotate(glm::mat4(1.0f),6.0f,glm::vec3(1.0f, 1.0f, 1.0));
+    glm::mat4x4 cubeScaleMat=glm::scale(glm::mat4(1.0f),glm::vec3(0.4f, 0.4f, 0.5));
+
+    cubeMat = cubeTransMat * cubeRotMat * cubeScaleMat;
+    glLoadMatrixf(glm::value_ptr(cubeMat));
+
+    glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
