f32643f4a84c426414dda2386b057ee6c2440428
liupanfeng <liupanfeng529@163.com>
2022-10-21 11:26:24 +0800

    这部分依然采用固定编程管线，接下来另外一个工程将采用可编程管线进行处理 VBO、EBO、VAO的使用也在另外一个工程

--- app/src/main/cpp/sample/MSTriangleSample.cpp
+++ app/src/main/cpp/sample/MSTriangleSample.cpp
@@ -32,51 +32,51 @@ MSTriangleSample::MSTriangleSample() {
         LOGE("m_AssetManager is null");
         return;
     }
-    m_msOpenGlShader = new MSOpenGLShader();
-    m_msOpenGlShader->InitShadersFromFile(m_AssetManager,"vertex.vert","fragment.frag");
+//    m_msOpenGlShader = new MSOpenGLShader();
+//    m_msOpenGlShader->InitShadersFromFile(m_AssetManager,"vertex.vert","fragment.frag");
 }
 
 MSTriangleSample::~MSTriangleSample() {
 }
 
 void MSTriangleSample::PaintGL(GLuint *m_texIDs) {
-//    fixedPipelinePaint();
+    fixedPipelinePaint();
 
-    MSFloat7 vertexTriangle[] ={
-            {- 0.5,-0.5,-1,1.0,0.0,0.0,1.0},
-            {0.5,-0.5,-1,0.0,1.0,0.0,1.0},
-            {0.0,0.5,-1,0.0,0.0,1.0,1.0},
-
-    };
-
-    mAngle += 0.01f;
-
-    glm::mat4x4  cubeMat;
-    glm::mat4x4  cubeTransMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3));
-    glm::mat4x4  cubeRotMat = glm::rotate(glm::mat4(1.0f),mAngle,glm::vec3(1.0f, 1.0f, 1.0) );
-    glm::mat4x4  cubeScaleMat = glm::scale(glm::mat4(1.0f),glm::vec3(0.3f, 0.2f, 0.3) );
-
-    glm::mat4 projMat = glm::perspective(glm::radians(60.0f), (float)9/(float)18, 0.1f, 1000.0f);
-    cubeMat = projMat* cubeTransMat ;
-
-    m_msOpenGlShader->Bind();
-    m_msOpenGlShader->SetUniformValue("u_mat",cubeMat);
-
-    m_msOpenGlShader->EnableAttributeArray("a_position");
-    m_msOpenGlShader->EnableAttributeArray("a_color");
-
-    m_msOpenGlShader->SetAttributeBuffer("a_position",GL_FLOAT,
-                                         vertexTriangle,3,sizeof (MSFloat7));
-
-    m_msOpenGlShader->SetAttributeBuffer("a_color",GL_FLOAT,
-                                         &vertexTriangle[0].r,4,sizeof(MSFloat7));
-
-    glDrawArrays(GL_TRIANGLES,0,3);
-
-    m_msOpenGlShader->DisableAttributeArray("a_position");
-    m_msOpenGlShader->DisableAttributeArray("a_color");
-
-    m_msOpenGlShader->Release();
+//    MSFloat7 vertexTriangle[] ={
+//            {- 0.5,-0.5,-1,1.0,0.0,0.0,1.0},
+//            {0.5,-0.5,-1,0.0,1.0,0.0,1.0},
+//            {0.0,0.5,-1,0.0,0.0,1.0,1.0},
+//
+//    };
+//
+//    mAngle += 0.01f;
+//
+//    glm::mat4x4  cubeMat;
+//    glm::mat4x4  cubeTransMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3));
+//    glm::mat4x4  cubeRotMat = glm::rotate(glm::mat4(1.0f),mAngle,glm::vec3(1.0f, 1.0f, 1.0) );
+//    glm::mat4x4  cubeScaleMat = glm::scale(glm::mat4(1.0f),glm::vec3(0.3f, 0.2f, 0.3) );
+//
+//    glm::mat4 projMat = glm::perspective(glm::radians(60.0f), (float)9/(float)18, 0.1f, 1000.0f);
+//    cubeMat = projMat* cubeTransMat ;
+//
+//    m_msOpenGlShader->Bind();
+//    m_msOpenGlShader->SetUniformValue("u_mat",cubeMat);
+//
+//    m_msOpenGlShader->EnableAttributeArray("a_position");
+//    m_msOpenGlShader->EnableAttributeArray("a_color");
+//
+//    m_msOpenGlShader->SetAttributeBuffer("a_position",GL_FLOAT,
+//                                         vertexTriangle,3,sizeof (MSFloat7));
+//
+//    m_msOpenGlShader->SetAttributeBuffer("a_color",GL_FLOAT,
+//                                         &vertexTriangle[0].r,4,sizeof(MSFloat7));
+//
+//    glDrawArrays(GL_TRIANGLES,0,3);
+//
+//    m_msOpenGlShader->DisableAttributeArray("a_position");
+//    m_msOpenGlShader->DisableAttributeArray("a_color");
+//
+//    m_msOpenGlShader->Release();
 
 }
 
